6f8708cdb79eb0a19eae52eca99d957e0904d080
Kenz <kenz@yahoo.com>
2025-09-25 16:47:23 +0530

    Updated Prajwal.java

--- Prajwal.java
+++ Prajwal.java
@@ -5,6 +5,11 @@ public class Prajwal {
     
 }
 class Manjunath{
+    /*
+    can't make Manjunath public as,
+    in one .java file only one class that contain main() can be made public
+    for jvm to easily identify the main()
+    */
     void disp(){
         System.out.println("Manjunath");
     }
